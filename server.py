b0b38975ef7b882e6491d30333dacf6d3dc220de
Sadie Forbes <sadieforbes44@gmail.com>
2024-02-22 15:45:48 -0600

    testing pydealer

--- server.py
+++ server.py
@@ -1,9 +1,25 @@
 from flask import Flask
+import pydealer as dealer
+from pydealer.const import POKER_RANKS
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return "Hello world!"
+    return "The backend is running for your poker game!"
+
+@app.route("/welcome", methods=["GET"])
+def welcome():
+    return "Welcome!"
+
+@app.route("/gethand", methods=["GET"])
+def gethand():
+    deck = dealer.Deck()
+    deck.ranks = POKER_RANKS
+    deck.shuffle()
+    hand = deck.deal(7)
+    hand.sort()
+    return str(hand)
 
 if (__name__ == "__main__"):
     app.run()
